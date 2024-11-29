cace25bcc8f4a72f1459c779a96688fdb98abf49
Yanfeng Zhu <yzhu@walmartlabs.com>
2024-11-28 20:48:43 -0800

    disable csrf

--- src/main/java/com/exmek/core/context/SecurityConfiguration.java
+++ src/main/java/com/exmek/core/context/SecurityConfiguration.java
@@ -1,21 +1,16 @@
 package com.exmek.core.context;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.exmek.core.config.AppConfig;
 import com.exmek.core.filter.ConsumerAuthFilter;
@@ -35,23 +30,27 @@ public class SecurityConfiguration implements ApplicationContextAware {
 	}
 	  
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity httpSec) throws Exception {
     	ConsumerAuthFilter consumerAuthFilter = applicationContext.getBean(ConsumerAuthFilter.class);
-    	http.addFilterBefore(consumerAuthFilter, UsernamePasswordAuthenticationFilter.class);
-    	http.cors(Customizer.withDefaults());
-    	return http.build();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(appConfig.getCorsAllowedOrigins());
-        corsConfig.setAllowedMethods(Arrays.asList(
-        		HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name()));
-        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "*"));
-        corsConfig.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-        return source;
+    	httpSec
+    	.addFilterBefore(consumerAuthFilter, UsernamePasswordAuthenticationFilter.class)
+    	.cors(cors -> cors.configurationSource(request -> {
+            CorsConfiguration corsConfig = new CorsConfiguration();
+            corsConfig.setAllowedOrigins(appConfig.getCorsAllowedOrigins());
+            corsConfig.addAllowedMethod("*");
+            corsConfig.addAllowedHeader("*");
+            corsConfig.setAllowCredentials(true);
+            return corsConfig;
+        }))
+    	.csrf(csrf -> csrf.disable())
+    	.authorizeHttpRequests(auth -> {
+    	    auth
+    	    .requestMatchers("/api/**")
+    	    .permitAll()
+    	    .anyRequest()
+    	    .authenticated();
+    	})
+    	.httpBasic(Customizer.withDefaults());
+    	return httpSec.build();
     }
 }
