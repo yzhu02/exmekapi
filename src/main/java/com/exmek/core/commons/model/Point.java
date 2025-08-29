d1ffa5c6c8ee5dafb39d6ec05206a56fa4afdd34
Yanfeng Zhu <yzhu@walmartlabs.com>
2025-08-29 10:45:58 -0700

    Add Include.NON_NULL annotation to Point

--- src/main/java/com/exmek/core/commons/model/Point.java
+++ src/main/java/com/exmek/core/commons/model/Point.java
@@ -1,8 +1,11 @@
 package com.exmek.core.commons.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Point<T extends Number> {
 
 	private T x;
